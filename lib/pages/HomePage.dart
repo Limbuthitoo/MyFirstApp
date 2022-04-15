743eb91b938723894053503cdd7e1cbd3f12b722
Bijay Subba Limbu <bijaysubbalimbu@gmail.com>
2022-04-15 22:04:43 +0545

    First Commit

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int count = 30;
+  final String name = 'Bijay';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("$name is doing $count App tutorials"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
